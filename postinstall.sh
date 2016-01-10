97346acd0ac6b31d9ac7729ffaa0bca96a95572d
Dmytro <Porreh@gmail.com>
2016-01-10 10:07:54 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
-# -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# -*- Mode: sh; coding: utf-8 -*-
+
+#       Install fonts
+
+wget -O- "https://googledrive.com/host/0B1Nw7439j8LSdU9IeHVxX3lDNlk" | sudo tar xvz -C /usr/share/fonts/truetype
+wget -O- "https://googledrive.com/host/0B1Nw7439j8LSdHhnVnpIRXUwRFU" | sudo tar xvz -C /usr/share/fonts/truetype
+
+#       Updating
 
 sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get upgrade
 
-#...................................#
-#         Add Repositories          #
-#...................................#
+#       Add repositories
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
-sudo add-apt-repository ppa:elementary-os/daily
-sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
-sudo apt-add-repository ppa:ricotz/docky
-sudo add-apt-repository ppa:evolve-os/ppa
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo add-apt-repository ppa:videolan/stable-daily
-sudo add-apt-repository ppa:transmissionbt/ppa
+
+sudo add-apt-repository ppa:ravefinity-project/ppa
+sudo add-apt-repository ppa:snwh/pulp
 
 sudo apt-get update
 
-#.......................................#
-#         Install applications          #
-#.......................................#
+#       Install applications
 
-sudo apt-get install vlc transmission atom ubuntu-restricted-extras google-chrome-stable
+sudo apt-get install vlc transmission ubuntu-restricted-extras inkscape gimp conky git google-chrome-stable paper-gtk-theme paper-icon-theme ambiance-flat-colors radiance-flat-colors lib32z1 lib32ncurses5 lib32stdc++6
 
-#.......................................#
-#               Cleaning                #
-#.......................................#
+#       Cleaning
 
 sudo apt-get autoremove
 sudo apt-get clean
